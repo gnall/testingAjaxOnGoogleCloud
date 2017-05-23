033ac920f598bd3a10fad44081fc2d3c655e6273
josephgnall <joseph.gnall@scranton.edu>
2017-05-23 14:45:41 -0400

    test2

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class UserService {
